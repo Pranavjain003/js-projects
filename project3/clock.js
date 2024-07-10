4374787e1c4149289bdff54559768f748db1217f
Pranav Jain <114664433+Pranavjain003@users.noreply.github.com>
2024-07-10 05:26:45 +0000

    clock

--- project3/clock.js
+++ project3/clock.js
@@ -4,6 +4,6 @@ const clock = document.getElementById('clock')
 
 setInterval(function(){ 
     let date = new Date();
-    clock.innerHTML = date.toLocaleTimeString();
+    clock.innerHTML = `${date.toLocaleTimeString()} & ${date.toLocaleDateString()}`;
 //  console.log( date.toLocaleTimeString())
 },1000)
